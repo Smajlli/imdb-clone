4207ec25c766570cbe24ed03f265e69f28bbde7b
Smajli3 <amer.smajli@icloud.com>
2023-12-19 16:06:38 +0100

    Update component -> Add classnames npm package and search filter elements

--- src/navbar/SearchFilter.jsx
+++ src/navbar/SearchFilter.jsx
@@ -1,135 +1,165 @@
 import "./SearchFilter.css"
+import classNames from "classnames";
+import { useState } from "react";
 
 function SearchFilter() {
+    const [searchFilter, setSearchFilter] = useState(false);
+    const searchFilterClasses = classNames('Search-filter', {'activeSearchFilter' : searchFilter === true});
+
+    const handleSearchFilter = () => {
+        setSearchFilter(!searchFilter);
+    }
+
     return(
-        <div className="Search-filter">
-            <div className="Filter-container">
-                <div className="Filter-category">
-                    <svg
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path
-                            fillRule="evenodd"
-                            clipRule="evenodd"
-                            d="M18.319 14.4326C20.7628 11.2941 20.542 6.75347 17.6569 3.86829C14.5327 0.744098 9.46734 0.744098 6.34315 3.86829C3.21895 6.99249 3.21895 12.0578 6.34315 15.182C9.22833 18.0672 13.769 18.2879 16.9075 15.8442C16.921 15.8595 16.9351 15.8745 16.9497 15.8891L21.1924 20.1317C21.5829 20.5223 22.2161 20.5223 22.6066 20.1317C22.9971 19.7412 22.9971 19.1081 22.6066 18.7175L18.364 14.4749C18.3493 14.4603 18.3343 14.4462 18.319 14.4326ZM16.2426 5.28251C18.5858 7.62565 18.5858 11.4246 16.2426 13.7678C13.8995 16.1109 10.1005 16.1109 7.75736 13.7678C5.41421 11.4246 5.41421 7.62565 7.75736 5.28251C10.1005 2.93936 13.8995 2.93936 16.2426 5.28251Z"
-                            fill="currentColor"
-                        />
-                    </svg>
-                    <p className="Category-text">All</p>
-                </div>
-                <div className="Filter-category">
-                    <svg
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path
-                            d="M6 7C5.44772 7 5 7.44772 5 8C5 8.55228 5.44772 9 6 9C6.55228 9 7 8.55228 7 8C7 7.44772 6.55228 7 6 7Z"
-                            fill="currentColor"
-                        />
-                        <path
-                            d="M17 8C17 7.44772 17.4477 7 18 7C18.5523 7 19 7.44772 19 8C19 8.55228 18.5523 9 18 9C17.4477 9 17 8.55228 17 8Z"
-                            fill="currentColor"
-                        />
-                        <path
-                            d="M6 11C5.44772 11 5 11.4477 5 12C5 12.5523 5.44772 13 6 13C6.55228 13 7 12.5523 7 12C7 11.4477 6.55228 11 6 11Z"
-                            fill="currentColor"
-                        />
-                        <path
-                            d="M17 12C17 11.4477 17.4477 11 18 11C18.5523 11 19 11.4477 19 12C19 12.5523 18.5523 13 18 13C17.4477 13 17 12.5523 17 12Z"
-                            fill="currentColor"
-                        />
-                        <path
-                            d="M6 15C5.44772 15 5 15.4477 5 16C5 16.5523 5.44772 17 6 17C6.55228 17 7 16.5523 7 16C7 15.4477 6.55228 15 6 15Z"
-                            fill="currentColor"
-                        />
-                        <path
-                            d="M17 16C17 15.4477 17.4477 15 18 15C18.5523 15 19 15.4477 19 16C19 16.5523 18.5523 17 18 17C17.4477 17 17 16.5523 17 16Z"
-                            fill="currentColor"
-                        />
-                        <path
-                            fillRule="evenodd"
-                        
-                            d="M4 3C2.34315 3 1 4.34315 1 6V18C1 19.6569 2.34315 21 4 21H20C21.6569 21 23 19.6569 23 18V6C23 4.34315 21.6569 3 20 3H4ZM20 5H4C3.44772 5 3 5.44772 3 6V18C3 18.5523 3.44772 19 4 19H20C20.5523 19 21 18.5523 21 18V6C21 5.44772 20.5523 5 20 5Z"
-                            fill="currentColor"
-                        />
-                    </svg>
-                    <p className="Category-text">Titles</p>
-                </div>
-                <div className="Filter-category">
-                    <svg
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path
-                            fillRule="evenodd"
-                            clipRule="evenodd"
-                            d="M8 6.11861L9.4131 4.70551L11.5371 6.82954L14.3667 4L15.7782 5.41151L13.3137 7.87598H18C19.1046 7.87598 20 8.77141 20 9.87598V16.876C20 17.9805 19.1046 18.876 18 18.876H6C4.89543 18.876 4 17.9805 4 16.876V9.87598C4 8.77141 4.89543 7.87598 6 7.87598H9.75736L8 6.11861ZM18 9.87598H6V16.876H18V9.87598Z"
-                            fill="currentColor"
-                        />
-                        <path d="M8 19.876H16V20.876H8V19.876Z" fill="currentColor" />
-                    </svg>
-                    <p className="Category-text">TV Episodes</p>
-                </div>
-                <div className="Filter-category">
-                    <svg
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path
-                            d="M9 14C9.55228 14 10 13.5523 10 13C10 12.4477 9.55228 12 9 12C8.44771 12 8 12.4477 8 13C8 13.5523 8.44771 14 9 14Z"
-                            fill="currentColor"
-                        />
-                        <path
-                            d="M16 13C16 13.5523 15.5523 14 15 14C14.4477 14 14 13.5523 14 13C14 12.4477 14.4477 12 15 12C15.5523 12 16 12.4477 16 13Z"
-                            fill="currentColor"
-                        />
-                        <path
-                            fillRule="evenodd"
-                            clipRule="evenodd"
-                            d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22ZM12 20C16.4183 20 20 16.4183 20 12C20 11.1637 19.8717 10.3574 19.6337 9.59973C18.7991 9.82556 17.9212 9.94604 17.0152 9.94604C13.2921 9.94604 10.0442 7.91139 8.32277 4.89334C5.75469 6.22486 4 8.90751 4 12C4 16.4183 7.58172 20 12 20Z"
-                            fill="currentColor"
-                        />
-                    </svg>
-                    <p className="Category-text">Celebs</p>
-                </div>
-                <div className="Filter-category">
-                    <svg
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path d="M17 5H7V7H17V5Z" fill="currentColor" />
-                        <path d="M7 9H9V11H7V9Z" fill="currentColor" />
-                        <path d="M9 13H7V15H9V13Z" fill="currentColor" />
-                        <path d="M7 17H9V19H7V17Z" fill="currentColor" />
-                        <path d="M13 9H11V11H13V9Z" fill="currentColor" />
-                        <path d="M11 13H13V15H11V13Z" fill="currentColor" />
-                        <path d="M13 17H11V19H13V17Z" fill="currentColor" />
-                        <path d="M15 9H17V11H15V9Z" fill="currentColor" />
-                        <path d="M17 13H15V19H17V13Z" fill="currentColor" />
-                        <path
-                            fillRule="evenodd"
-                            clipRule="evenodd"
-                            d="M3 3C3 1.89543 3.89543 1 5 1H19C20.1046 1 21 1.89543 21 3V21C21 22.1046 20.1046 23 19 23H5C3.89543 23 3 22.1046 3 21V3ZM5 3H19V21H5V3Z"
-                            fill="currentColor"
-                        />
-                    </svg>
-                    <p className="Category-text">Companies</p>
+        <div className="Search-box">
+            <div className='Left-side-search' onClick={handleSearchFilter}><label htmlFor="search">All</label></div>
+            <div className='Right-side-search'><input type="text" className='Search' id='search' placeholder='Search IMDb' /></div>
+            <a href="#" className='searchIconContainer'>
+                <svg
+                    width="22"
+                    height="22"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    className='searchIcon'
+                >
+                    <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M18.319 14.4326C20.7628 11.2941 20.542 6.75347 17.6569 3.86829C14.5327 0.744098 9.46734 0.744098 6.34315 3.86829C3.21895 6.99249 3.21895 12.0578 6.34315 15.182C9.22833 18.0672 13.769 18.2879 16.9075 15.8442C16.921 15.8595 16.9351 15.8745 16.9497 15.8891L21.1924 20.1317C21.5829 20.5223 22.2161 20.5223 22.6066 20.1317C22.9971 19.7412 22.9971 19.1081 22.6066 18.7175L18.364 14.4749C18.3493 14.4603 18.3343 14.4462 18.319 14.4326ZM16.2426 5.28251C18.5858 7.62565 18.5858 11.4246 16.2426 13.7678C13.8995 16.1109 10.1005 16.1109 7.75736 13.7678C5.41421 11.4246 5.41421 7.62565 7.75736 5.28251C10.1005 2.93936 13.8995 2.93936 16.2426 5.28251Z"
+                        fill="currentColor"
+                    />
+                </svg>
+            </a>
+            <div className={searchFilterClasses}>
+                <div className="Filter-container">
+                    <div className="Filter-category">
+                        <svg
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                fillRule="evenodd"
+                                clipRule="evenodd"
+                                d="M18.319 14.4326C20.7628 11.2941 20.542 6.75347 17.6569 3.86829C14.5327 0.744098 9.46734 0.744098 6.34315 3.86829C3.21895 6.99249 3.21895 12.0578 6.34315 15.182C9.22833 18.0672 13.769 18.2879 16.9075 15.8442C16.921 15.8595 16.9351 15.8745 16.9497 15.8891L21.1924 20.1317C21.5829 20.5223 22.2161 20.5223 22.6066 20.1317C22.9971 19.7412 22.9971 19.1081 22.6066 18.7175L18.364 14.4749C18.3493 14.4603 18.3343 14.4462 18.319 14.4326ZM16.2426 5.28251C18.5858 7.62565 18.5858 11.4246 16.2426 13.7678C13.8995 16.1109 10.1005 16.1109 7.75736 13.7678C5.41421 11.4246 5.41421 7.62565 7.75736 5.28251C10.1005 2.93936 13.8995 2.93936 16.2426 5.28251Z"
+                                fill="currentColor"
+                            />
+                        </svg>
+                        <p className="Category-text">All</p>
+                    </div>
+                    <div className="Filter-category">
+                        <svg
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                d="M6 7C5.44772 7 5 7.44772 5 8C5 8.55228 5.44772 9 6 9C6.55228 9 7 8.55228 7 8C7 7.44772 6.55228 7 6 7Z"
+                                fill="currentColor"
+                            />
+                            <path
+                                d="M17 8C17 7.44772 17.4477 7 18 7C18.5523 7 19 7.44772 19 8C19 8.55228 18.5523 9 18 9C17.4477 9 17 8.55228 17 8Z"
+                                fill="currentColor"
+                            />
+                            <path
+                                d="M6 11C5.44772 11 5 11.4477 5 12C5 12.5523 5.44772 13 6 13C6.55228 13 7 12.5523 7 12C7 11.4477 6.55228 11 6 11Z"
+                                fill="currentColor"
+                            />
+                            <path
+                                d="M17 12C17 11.4477 17.4477 11 18 11C18.5523 11 19 11.4477 19 12C19 12.5523 18.5523 13 18 13C17.4477 13 17 12.5523 17 12Z"
+                                fill="currentColor"
+                            />
+                            <path
+                                d="M6 15C5.44772 15 5 15.4477 5 16C5 16.5523 5.44772 17 6 17C6.55228 17 7 16.5523 7 16C7 15.4477 6.55228 15 6 15Z"
+                                fill="currentColor"
+                            />
+                            <path
+                                d="M17 16C17 15.4477 17.4477 15 18 15C18.5523 15 19 15.4477 19 16C19 16.5523 18.5523 17 18 17C17.4477 17 17 16.5523 17 16Z"
+                                fill="currentColor"
+                            />
+                            <path
+                                fillRule="evenodd"
+
+                                d="M4 3C2.34315 3 1 4.34315 1 6V18C1 19.6569 2.34315 21 4 21H20C21.6569 21 23 19.6569 23 18V6C23 4.34315 21.6569 3 20 3H4ZM20 5H4C3.44772 5 3 5.44772 3 6V18C3 18.5523 3.44772 19 4 19H20C20.5523 19 21 18.5523 21 18V6C21 5.44772 20.5523 5 20 5Z"
+                                fill="currentColor"
+                            />
+                        </svg>
+                        <p className="Category-text">Titles</p>
+                    </div>
+                    <div className="Filter-category">
+                        <svg
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                fillRule="evenodd"
+                                clipRule="evenodd"
+                                d="M8 6.11861L9.4131 4.70551L11.5371 6.82954L14.3667 4L15.7782 5.41151L13.3137 7.87598H18C19.1046 7.87598 20 8.77141 20 9.87598V16.876C20 17.9805 19.1046 18.876 18 18.876H6C4.89543 18.876 4 17.9805 4 16.876V9.87598C4 8.77141 4.89543 7.87598 6 7.87598H9.75736L8 6.11861ZM18 9.87598H6V16.876H18V9.87598Z"
+                                fill="currentColor"
+                            />
+                            <path d="M8 19.876H16V20.876H8V19.876Z" fill="currentColor" />
+                        </svg>
+                        <p className="Category-text">TV Episodes</p>
+                    </div>
+                    <div className="Filter-category">
+                        <svg
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                d="M9 14C9.55228 14 10 13.5523 10 13C10 12.4477 9.55228 12 9 12C8.44771 12 8 12.4477 8 13C8 13.5523 8.44771 14 9 14Z"
+                                fill="currentColor"
+                            />
+                            <path
+                                d="M16 13C16 13.5523 15.5523 14 15 14C14.4477 14 14 13.5523 14 13C14 12.4477 14.4477 12 15 12C15.5523 12 16 12.4477 16 13Z"
+                                fill="currentColor"
+                            />
+                            <path
+                                fillRule="evenodd"
+                                clipRule="evenodd"
+                                d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22ZM12 20C16.4183 20 20 16.4183 20 12C20 11.1637 19.8717 10.3574 19.6337 9.59973C18.7991 9.82556 17.9212 9.94604 17.0152 9.94604C13.2921 9.94604 10.0442 7.91139 8.32277 4.89334C5.75469 6.22486 4 8.90751 4 12C4 16.4183 7.58172 20 12 20Z"
+                                fill="currentColor"
+                            />
+                        </svg>
+                        <p className="Category-text">Celebs</p>
+                    </div>
+                    <div className="Filter-category">
+                        <svg
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path d="M17 5H7V7H17V5Z" fill="currentColor" />
+                            <path d="M7 9H9V11H7V9Z" fill="currentColor" />
+                            <path d="M9 13H7V15H9V13Z" fill="currentColor" />
+                            <path d="M7 17H9V19H7V17Z" fill="currentColor" />
+                            <path d="M13 9H11V11H13V9Z" fill="currentColor" />
+                            <path d="M11 13H13V15H11V13Z" fill="currentColor" />
+                            <path d="M13 17H11V19H13V17Z" fill="currentColor" />
+                            <path d="M15 9H17V11H15V9Z" fill="currentColor" />
+                            <path d="M17 13H15V19H17V13Z" fill="currentColor" />
+                            <path
+                                fillRule="evenodd"
+                                clipRule="evenodd"
+                                d="M3 3C3 1.89543 3.89543 1 5 1H19C20.1046 1 21 1.89543 21 3V21C21 22.1046 20.1046 23 19 23H5C3.89543 23 3 22.1046 3 21V3ZM5 3H19V21H5V3Z"
+                                fill="currentColor"
+                            />
+                        </svg>
+                        <p className="Category-text">Companies</p>
+                    </div>
                 </div>
             </div>
         </div>
@@ -137,3 +167,8 @@ function SearchFilter() {
 }
 
 export default SearchFilter;
+
+
+
+
+
