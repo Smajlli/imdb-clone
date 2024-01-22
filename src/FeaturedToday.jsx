f12e7a8cc730b53783f582d208c0cd3d6399a23d
Smajli3 <amer.smajli@icloud.com>
2024-01-22 11:39:59 +0100

    Update component -> Fetch aditional data and edit carousel

--- src/FeaturedToday.jsx
+++ src/FeaturedToday.jsx
@@ -1,6 +1,15 @@
 import './FeaturedToday.css'
 import { useState, useEffect } from 'react';
 import screenSize from './hooks/ScreenSize';
+import FeaturedTodayCard from './FeaturedTodayCard';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/bundle';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 const apiOptions = {
     method: 'GET',
@@ -13,6 +22,9 @@ const apiOptions = {
 
 function FeaturedToday({stars}) {
     const[upcomingMovies, setUpcomingMovies] = useState([]);
+    const[shows, setShows] = useState([]);
+    const[theaters, setTheaters] = useState([]);
+
     const windowSize = screenSize()
 
     useEffect(() => {
@@ -24,10 +36,29 @@ function FeaturedToday({stars}) {
             setUpcomingMovies(upcomingMovies.results);
         }
         fetchUpcomingMovies();
+
+        async function fetchShows() {
+            const getShows = await fetch('https://api.themoviedb.org/3/trending/tv/day?language=en-US', apiOptions)
+            .catch(err => console.log(err))
+            const showsJson = await getShows.json();
+            const allShows = showsJson;
+            setShows(allShows.results);
+        }
+        fetchShows();
+
+        async function fetchTheaters() {
+            const getTheaters = await fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', apiOptions)
+            .catch(err => console.log(err))
+            const theatersJson = await getTheaters.json();
+            const allTheaters = theatersJson;
+            setTheaters(allTheaters.results)
+        }
+        fetchTheaters();
+
     }, []);
 
 
-    if(!upcomingMovies || upcomingMovies.length === 0 || !stars || stars.length === 0) {
+    if(!upcomingMovies || upcomingMovies.length === 0 || !stars || stars.length === 0 || !shows || shows.length === 0 || !theaters || theaters.length === 0) {
         return null 
     } else {
        if(windowSize.width > 1024 || windowSize.width === 1024) {
@@ -36,66 +67,19 @@ function FeaturedToday({stars}) {
                    <div className='featuredTodayHeading'>Featured today</div>
                    <div className='featuredTodayContentContainer'>
                        <div className="featuredTodayContentWrapper">
-                           <div className="featuredTodayContentLeft">
-                               <div className='featuredTodayContentImgGradient'></div>
-                               <div className='featuredTodayImgContainer'>
-                                   <img src={`https://image.tmdb.org/t/p/original${upcomingMovies[0].poster_path}`} alt="upcomingMovieImg" className='featuredTodayImgLeft' />
-                                   <img src={`https://image.tmdb.org/t/p/original${upcomingMovies[1].poster_path}`} alt="upcomingMovieImg" className='featuredTodayImgMiddle' />
-                                   <img src={`https://image.tmdb.org/t/p/original${upcomingMovies[2].poster_path}`} alt="upcomingMovieImg" className='featuredTodayImgRight' />
-                                   <div className='featuredTodayContentList'>
-                                       <svg
-                                           width="45"
-                                           height="45"
-                                           viewBox="0 0 24 24"
-                                           fill="none"
-                                           xmlns="http://www.w3.org/2000/svg"
-                                           className='featuredTodayContentListIcon'
-                                       >
-                                           <path d="M9 7H7V9H9V7Z" fill="currentColor" />
-                                           <path d="M7 13V11H9V13H7Z" fill="currentColor" />
-                                           <path d="M7 15V17H9V15H7Z" fill="currentColor" />
-                                           <path d="M11 15V17H17V15H11Z" fill="currentColor" />
-                                           <path d="M17 13V11H11V13H17Z" fill="currentColor" />
-                                           <path d="M17 7V9H11V7H17Z" fill="currentColor" />
-                                       </svg>
-                                       <div className='featuredTodayContentListText'>List</div>
-                                   </div>
-                               </div>
-                               <div className='featuredTodayContentTextContainer'>
-                                   <div className='featuredTodayContentTitle'>Upcoming movies</div>
-                                   <div className='featuredTodayContentSubtitle'>See all upcoming movies</div>
-                               </div>
-                           </div>
-                           <div className="featuredTodayContentRight">
-                               <div className='featuredTodayContentImgGradient'></div>
-                               <div className='featuredTodayImgContainer'>
-                                   <img src={`https://image.tmdb.org/t/p/original${stars[0].profile_path}`} alt="actorImage" className='featuredTodayImgLeft' />
-                                   <img src={`https://image.tmdb.org/t/p/original${stars[1].profile_path}`} alt="actorImage" className='featuredTodayImgMiddle' />
-                                   <img src={`https://image.tmdb.org/t/p/original${stars[2].profile_path}`} alt="actorImage" className='featuredTodayImgRight' />
-                                   <div className='featuredTodayContentList'>
-                                       <svg
-                                           width="45"
-                                           height="45"
-                                           viewBox="0 0 24 24"
-                                           fill="none"
-                                           xmlns="http://www.w3.org/2000/svg"
-                                           className='featuredTodayContentListIcon'
-                                       >
-                                           <path d="M9 7H7V9H9V7Z" fill="currentColor" />
-                                           <path d="M7 13V11H9V13H7Z" fill="currentColor" />
-                                           <path d="M7 15V17H9V15H7Z" fill="currentColor" />
-                                           <path d="M11 15V17H17V15H11Z" fill="currentColor" />
-                                           <path d="M17 13V11H11V13H17Z" fill="currentColor" />
-                                           <path d="M17 7V9H11V7H17Z" fill="currentColor" />
-                                       </svg>
-                                       <div className='featuredTodayContentListText'>List</div>
-                                   </div>
-                               </div>
-                               <div className='featuredTodayContentTextContainer'>
-                                   <div className='featuredTodayContentTitle'>The Top Stars of 2023</div>
-                                   <div className='featuredTodayContentSubtitle'>See top stars</div>
-                               </div>
-                           </div>
+                           <Swiper
+                               modules={[Navigation, Pagination, Scrollbar, A11y]}
+                               spaceBetween={50}
+                               width={450}
+                               slidesPerView={1}
+                               style={{ width: 1250, height: 490 }}
+                               navigation
+                           >
+                               <SwiperSlide><FeaturedTodayCard content={upcomingMovies} title={"Upcoming movies"} subTitle={"See all upcoming movies"} /></SwiperSlide>
+                               <SwiperSlide><FeaturedTodayCard content={stars} title={"Popular Actors"} subTitle={"See all trending actors"} /></SwiperSlide>
+                               <SwiperSlide><FeaturedTodayCard content={shows} title={"Trending TV Shows"} subTitle={"See poprular TV Shows for today"} /></SwiperSlide>
+                               <SwiperSlide><FeaturedTodayCard content={theaters} title={"Now Playing"} subTitle={"See what's currently in theaters"} /></SwiperSlide>
+                           </Swiper>  
                        </div>
                    </div>
                </div>
@@ -106,51 +90,16 @@ function FeaturedToday({stars}) {
                 <div className='featuredTodayHeading'>Featured today</div>
                 <div className='featuredTodayContentContainer'>
                     <div className="featuredTodayContentWrapper">
-                        <div className="featuredTodayContentRight">
-                            <div className='featuredTodayContentButtonContainer'>
-                                <svg
-                                    width="24"
-                                    height="24"
-                                    viewBox="0 0 24 24"
-                                    fill="none"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    className='featuredTodayContentArrowIcon'
-                                >
-                                    <path
-                                        d="M15.0378 6.34317L13.6269 7.76069L16.8972 11.0157L3.29211 11.0293L3.29413 13.0293L16.8619 13.0157L13.6467 16.2459L15.0643 17.6568L20.7079 11.9868L15.0378 6.34317Z"
-                                        fill="currentColor"
-                                    />
-                                </svg>
-                            </div>
-                            <div className='featuredTodayContentImgGradient'></div>
-                            <div className='featuredTodayImgContainer'>
-                                <img src={`https://image.tmdb.org/t/p/original${stars[0].profile_path}`} alt="actorImage" className='featuredTodayImgLeft' />
-                                <img src={`https://image.tmdb.org/t/p/original${stars[1].profile_path}`} alt="actorImage" className='featuredTodayImgMiddle' />
-                                <img src={`https://image.tmdb.org/t/p/original${stars[2].profile_path}`} alt="actorImage" className='featuredTodayImgRight' />
-                                <div className='featuredTodayContentList'>
-                                    <svg
-                                        width="45"
-                                        height="45"
-                                        viewBox="0 0 24 24"
-                                        fill="none"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        className='featuredTodayContentListIcon'
-                                    >
-                                        <path d="M9 7H7V9H9V7Z" fill="currentColor" />
-                                        <path d="M7 13V11H9V13H7Z" fill="currentColor" />
-                                        <path d="M7 15V17H9V15H7Z" fill="currentColor" />
-                                        <path d="M11 15V17H17V15H11Z" fill="currentColor" />
-                                        <path d="M17 13V11H11V13H17Z" fill="currentColor" />
-                                        <path d="M17 7V9H11V7H17Z" fill="currentColor" />
-                                    </svg>
-                                    <div className='featuredTodayContentListText'>List</div>
-                                </div>
-                            </div>
-                            <div className='featuredTodayContentTextContainer'>
-                                <div className='featuredTodayContentTitle'>The Top Stars of 2023</div>
-                                <div className='featuredTodayContentSubtitle'>See top stars</div>
-                            </div>
-                        </div>
+                        <Swiper
+                            modules={[Navigation, Pagination, Scrollbar, A11y]}
+                            spaceBetween={50}
+                            width={500}
+                            slidesPerView={1}
+                            style={{ width: 1250, height: 490 }}
+                            navigation
+                        >
+                            <SwiperSlide><FeaturedTodayCard content={upcomingMovies} title={"Upcoming movies"} subTitle={"See all upcoming movies"} /></SwiperSlide>
+                        </Swiper>
                     </div>
                 </div>
             </div>
