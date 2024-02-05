31551a85cd1f3862ea7b2968567737e564449638
Smajli3 <amer.smajli@icloud.com>
2024-02-05 15:58:54 +0100

    Update component -> Fix slice problem and add additional elements and styles

--- src/BoxOfficeCard.jsx
+++ src/BoxOfficeCard.jsx
@@ -1,10 +1,17 @@
 import './BoxOfficeCard.css';
 
-function BoxOfficeCard({title, revenue}) {
+function BoxOfficeCard({title, revenue, number}) {
+    const stringRevenue = revenue.toString()
     return(
         <div className="boxOfficeCardContainer">
-                <div className='boxOfficeCardTitle'>{title}</div>
-                <div className="boxOfficeCardRevenue">{revenue.slice(0, 3)}</div>
+                <div className="boxOfficeNumber">{number}</div>
+                <div className='boxOfficeBookmark'>
+                    <div className="boxOfficeBookmarkText">+</div>
+                </div>
+                <div className="boxOfficeInnerContent">
+                    <div className='boxOfficeCardTitle'>{title}</div>
+                    <div className="boxOfficeCardRevenue">${stringRevenue.slice(0, 3)} M</div>
+                </div>
         </div>
     )
 }
