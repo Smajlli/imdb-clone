a7e8934e386590ecff43c32293329787e60189a3
Smajli3 <amer.smajli@icloud.com>
2024-02-05 15:57:26 +0100

    Update component -> Fix bugs and add additional elements and styles

--- src/BoxOffice.jsx
+++ src/BoxOffice.jsx
@@ -49,22 +49,24 @@ function BoxOffice() {
     if(movieRevenue.length === 0 ) {
         return null 
     } else {
-       <div className='boxOfficeContainer'>
-            <div className='boxOfficeHeading'>Explore Movies and TV Shows</div>
-            <div className='boxOfficeSubheading'>Top box office US</div>
-            <div className='boxOfficeCardContainer'>
-                <div className="boxOfficeCardContentLeft">
-                    <BoxOfficeCard title={movieRevenue[1].original_title} revenue={movieRevenue[1].revenue} />
-                    <BoxOfficeCard title={movieRevenue[2].original_title} revenue={movieRevenue[2].revenue} />
-                    <BoxOfficeCard title={movieRevenue[3].original_title} revenue={movieRevenue[3].revenue} />
-                </div>
-                <div className='boxOfficeCardContentRight'>
-                    <BoxOfficeCard title={movieRevenue[4].original_title} revenue={movieRevenue[4].revenue} />
-                    <BoxOfficeCard title={movieRevenue[5].original_title} revenue={movieRevenue[5].revenue} />
-                    <BoxOfficeCard title={movieRevenue[6].original_title} revenue={movieRevenue[6].revenue} />
-                </div>
-            </div>
-        </div>
+       return(
+           <div className='boxOfficeContainer'>
+               <div className='boxOfficeHeading'>Explore Movies and TV Shows</div>
+               <div className='boxOfficeSubheading'>Top box office US</div>
+               <div className='boxOfficeCardContainer'>
+                   <div className="boxOfficeCardContentLeft">
+                       <BoxOfficeCard title={movieRevenue[1].original_title} revenue={movieRevenue[1].revenue} number={1}/>
+                       <BoxOfficeCard title={movieRevenue[2].original_title} revenue={movieRevenue[2].revenue} number={2}/>
+                       <BoxOfficeCard title={movieRevenue[3].original_title} revenue={movieRevenue[3].revenue} number={3}/>
+                   </div>
+                   <div className='boxOfficeCardContentRight'>
+                       <BoxOfficeCard title={movieRevenue[4].original_title} revenue={movieRevenue[4].revenue} number={4}/>
+                       <BoxOfficeCard title={movieRevenue[5].original_title} revenue={movieRevenue[5].revenue} number={5}/>
+                       <BoxOfficeCard title={movieRevenue[6].original_title} revenue={movieRevenue[6].revenue} number={6}/>
+                   </div>
+               </div>
+           </div>
+       )
     } 
 
 }
