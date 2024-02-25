02c4e51c662cb90f04aec8d2537c7ae682527c97
Smajli3 <amer.smajli@icloud.com>
2024-02-25 14:21:51 +0100

    Update component -> Make component responsive on 1024, 768 and 425 breakpoints

--- src/Upcoming.jsx
+++ src/Upcoming.jsx
@@ -1,6 +1,15 @@
 import './Upcoming.css'
 import { useState, useEffect } from 'react';
 import screenSize from './hooks/ScreenSize';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/bundle';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import UpcomingCard from './UpcomingCard';
 
 const apiOptions = {
     method: 'GET', 
@@ -28,7 +37,7 @@ function Upcoming() {
     if(!upcoming || upcoming.length === 0) {
         return null 
     } else {
-        if(windowSize.width > 1025) {
+        if(windowSize.width >= 1024) {
             return <>
                 <div className='upcomingContainer'>
                     <div className='upcomingHeading'>Coming soon to theaters</div>
@@ -109,109 +118,66 @@ function Upcoming() {
                     </div>
                 </div>
             </>
-        } else if(windowSize.width > 768) {
+        } else if(windowSize.width >= 768) {
             return <>
                 <div className='upcomingContainer'>
                     <div className='upcomingHeading'>Coming soon to theaters</div>
                     <div className='upcomingSubheading'>Trailers for upcoming releases</div>
-                    <div className='upcomingContentContainer'>
-                        <div className='upcomingContentElement'>
-                            <div className='upcomingImgContainer'>
-                                <svg
-                                    width="28"
-                                    height="28"
-                                    viewBox="0 0 24 24"
-                                    fill="none"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    className='upcomingPlayIcon'
-                                >
-                                    <path
-                                        fillRule="evenodd"
-                                        clipRule="evenodd"
-                                        d="M12 21C16.9706 21 21 16.9706 21 12C21 7.02944 16.9706 3 12 3C7.02944 3 3 7.02944 3 12C3 16.9706 7.02944 21 12 21ZM12 23C18.0751 23 23 18.0751 23 12C23 5.92487 18.0751 1 12 1C5.92487 1 1 5.92487 1 12C1 18.0751 5.92487 23 12 23Z"
-                                        fill="currentColor"
-                                    />
-                                    <path d="M16 12L10 16.3301V7.66987L16 12Z" fill="currentColor" />
-                                </svg>
-                                <div className='upcomingImgGradient'></div>
-                                <img src={`https://image.tmdb.org/t/p/original${upcoming[0].poster_path}`} alt='upcomingMovieImg' />
+                    <Swiper
+                        modules={[Navigation, Pagination, Scrollbar, A11y]}
+                        spaceBetween={50}
+                        slidesPerView={1}
+                        style={{ width: 500, height: 400 }}
+                        navigation
+                        loop
+                    >
+                        <SwiperSlide>
+                            <div className='upcomingCardContainer'>
+                                <UpcomingCard upcomingImg={upcoming[0].poster_path} title={upcoming[0].title} />
                             </div>
-                            <div className='upcomingContentTitle'>{upcoming[0].title}</div>
-                            <div className='upcomingContentSubtitle'>Watch trailer</div>
-                        </div>
-                        <div className='upcomingContentElement'>
-                            <div className='upcomingImgContainer'>
-                                <svg
-                                    width="28"
-                                    height="28"
-                                    viewBox="0 0 24 24"
-                                    fill="none"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    className='upcomingPlayIcon'
-                                >
-                                    <path
-                                        fillRule="evenodd"
-                                        clipRule="evenodd"
-                                        d="M12 21C16.9706 21 21 16.9706 21 12C21 7.02944 16.9706 3 12 3C7.02944 3 3 7.02944 3 12C3 16.9706 7.02944 21 12 21ZM12 23C18.0751 23 23 18.0751 23 12C23 5.92487 18.0751 1 12 1C5.92487 1 1 5.92487 1 12C1 18.0751 5.92487 23 12 23Z"
-                                        fill="currentColor"
-                                    />
-                                    <path d="M16 12L10 16.3301V7.66987L16 12Z" fill="currentColor" />
-                                </svg>
-                                <div className='upcomingImgGradient'></div>
-                                <img src={`https://image.tmdb.org/t/p/original${upcoming[1].poster_path}`} alt='upcomingMovieImg' />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className='upcomingCardContainer'>
+                                <UpcomingCard upcomingImg={upcoming[1].poster_path} title={upcoming[1].title} />
                             </div>
-                            <div className='upcomingContentTitle'>{upcoming[1].title}</div>
-                            <div className='upcomingContentSubtitle'>Watch trailer</div>
-                        </div>
-                    </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className='upcomingCardContainer'>
+                                <UpcomingCard upcomingImg={upcoming[2].poster_path} title={upcoming[2].title} />
+                            </div>
+                        </SwiperSlide>
+                    </Swiper>
                 </div>
             </>
-        } else {
+        } else if(windowSize.width <= 425) {
             return <>
                 <div className='upcomingContainer'>
                     <div className='upcomingHeading'>Coming soon to theaters</div>
                     <div className='upcomingSubheading'>Trailers for upcoming releases</div>
-                    <div className='upcomingContentContainer'>
-                        <div className='upcomingContentElement'>
-                            <div className='upcomingImgContainer'>
-                                <div className='upcomingImgContainerButton'>
-                                    <svg
-                                        width="24"
-                                        height="24"
-                                        viewBox="0 0 24 24"
-                                        fill="none"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        className='upcomingImgContainerButtonIcon'
-                                    >
-                                        <path
-                                            d="M15.0378 6.34317L13.6269 7.76069L16.8972 11.0157L3.29211 11.0293L3.29413 13.0293L16.8619 13.0157L13.6467 16.2459L15.0643 17.6568L20.7079 11.9868L15.0378 6.34317Z"
-                                            fill="currentColor"
-                                        />
-                                    </svg>
-                                </div>
-                                <svg
-                                    width="28"
-                                    height="28"
-                                    viewBox="0 0 24 24"
-                                    fill="none"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    className='upcomingPlayIcon'
-                                >
-                                    <path
-                                        fillRule="evenodd"
-                                        clipRule="evenodd"
-                                        d="M12 21C16.9706 21 21 16.9706 21 12C21 7.02944 16.9706 3 12 3C7.02944 3 3 7.02944 3 12C3 16.9706 7.02944 21 12 21ZM12 23C18.0751 23 23 18.0751 23 12C23 5.92487 18.0751 1 12 1C5.92487 1 1 5.92487 1 12C1 18.0751 5.92487 23 12 23Z"
-                                        fill="currentColor"
-                                    />
-                                    <path d="M16 12L10 16.3301V7.66987L16 12Z" fill="currentColor" />
-                                </svg>
-                                <div className='upcomingImgGradient'></div>
-                                <img src={`https://image.tmdb.org/t/p/original${upcoming[0].poster_path}`} alt='upcomingMovieImg' />
+                    <Swiper
+                        modules={[Navigation, Pagination, Scrollbar, A11y]}
+                        spaceBetween={50}
+                        slidesPerView={1}
+                        style={{ width: 420, height: 400 }}
+                        navigation
+                        loop
+                    >
+                        <SwiperSlide>
+                            <div className='upcomingCardContainer'>
+                                <UpcomingCard upcomingImg={upcoming[0].poster_path} title={upcoming[0].title} />
                             </div>
-                            <div className='upcomingContentTitle'>{upcoming[0].title}</div>
-                            <div className='upcomingContentSubtitle'>Watch trailer</div>
-                        </div>
-                    </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className='upcomingCardContainer'>
+                                <UpcomingCard upcomingImg={upcoming[1].poster_path} title={upcoming[1].title} />
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className='upcomingCardContainer'>
+                                <UpcomingCard upcomingImg={upcoming[2].poster_path} title={upcoming[2].title} />
+                            </div>
+                        </SwiperSlide>
+                    </Swiper>
                 </div>
             </>
         }
