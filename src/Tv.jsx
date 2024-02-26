0cc1ee52d2b4c27bf8e479c2642409f444ebb745
Smajli3 <amer.smajli@icloud.com>
2024-02-26 17:09:18 +0100

    Update component -> Make component responsive on 1024, 768 and 425 breakpoints

--- src/Tv.jsx
+++ src/Tv.jsx
@@ -9,6 +9,7 @@ import 'swiper/css/bundle';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
+import screenSize from './hooks/ScreenSize';
 
 const apiOptions = {
     method: 'GET',
@@ -20,6 +21,7 @@ const apiOptions = {
 
 function Tv() {
     const [tv, setTv] = useState([]);
+    const windowSize = screenSize();
 
     useEffect(() => {
         async function fetchTv() {
@@ -35,30 +37,103 @@ function Tv() {
     if(!tv && tv.length === 0) {
         return null 
     } else {
-        return(
-            <div className='whatToWatchContainer'>
-                <div className="whatToWatchContentContainer">
-                    <div className='whatToWatchTitle'>Explore what's streaming</div>
-                    <div className="whatToWatchContentWrapper">
-                        <div className="whatToWatchMovieCardContainer">
-                            <Swiper
-                                modules={[Navigation, Pagination, Scrollbar, A11y]}
-                                spaceBetween={0}
-                                width={210}
-                                slidesPerView={1}
-                                style={{ width: 1250, height: 490 }}
-                                navigation
-                            >
-                                {tv.map((show) => {
-                                    return <SwiperSlide><MovieCard key={show.id} movie={show} title={show.name} /></SwiperSlide>
-                                })}
-                            </Swiper>
+        const tvArray1 = [];
+        const tvArray2 = [];
+        const tvArray3 = [];
+
+        for(let i = 0; i < 6; i++) {
+            tvArray1.push(tv[i]);
+        }
+        for (let i = 6; i < 12; i++) {
+            tvArray2.push(tv[i]);
+        }
+        for (let i = 12; i < 18; i++) {
+            tvArray3.push(tv[i]);
+        }
+            return <>
+                <div className='nameContainer'>
+                    <div className='tvContainer'>
+                        <div className="tvContentContainer">
+                            <div className='tvTitle'>Explore what's streaming</div>
+                            <div className="tvContentWrapper">
+                                <div className="tvMovieCardWrapper">
+                                    {windowSize.width >= 1024 ? <Swiper
+                                        modules={[Navigation, Pagination, Scrollbar, A11y]}
+                                        spaceBetween={0}
+                                        slidesPerView={1}
+                                        style={{ width: 1250, height: 490 }}
+                                        navigation
+                                        loop
+                                    >
+                                        <SwiperSlide>
+                                            <div className='tvMovieCardsContainer'>
+                                                <div className='tvMovieCardsContainerContent'>
+                                                    {tvArray1.map((show) => {
+                                                        return <MovieCard key={show.id} movie={show} />
+                                                    })}
+                                                </div>
+                                            </div>
+                                        </SwiperSlide>
+                                        <SwiperSlide>
+                                            <div className='tvMovieCardsContainer'>
+                                                <div className='tvMovieCardsContainerContent'>
+                                                    {tvArray2.map((show) => {
+                                                        return <MovieCard key={show.id} movie={show} />
+                                                    })}
+                                                </div>
+                                            </div>
+                                        </SwiperSlide>
+                                        <SwiperSlide>
+                                            <div className='tvMovieCardsContainer'>
+                                                <div className='tvMovieCardsContainerContent'>
+                                                    {tvArray3.map((show) => {
+                                                        return <MovieCard key={show.id} movie={show} />
+                                                    })}
+                                                </div>
+                                            </div>
+                                        </SwiperSlide>
+                                    </Swiper> : windowSize.width === 768 ? <Swiper
+                                        modules={[Navigation, Pagination, Scrollbar, A11y]}
+                                        spaceBetween={0}
+                                        slidesPerView={4}
+                                        style={{ width: 768, height: 490 }}
+                                        loop
+                                    >
+                                        <SwiperSlide> <MovieCard movie={tv[0]} /> </SwiperSlide>
+                                        <SwiperSlide> <MovieCard movie={tv[1]} /> </SwiperSlide>
+                                        <SwiperSlide> <MovieCard movie={tv[2]} /> </SwiperSlide>
+                                        <SwiperSlide> <MovieCard movie={tv[3]} /> </SwiperSlide>
+                                        <SwiperSlide> <MovieCard movie={tv[4]} /> </SwiperSlide>
+                                        <SwiperSlide> <MovieCard movie={tv[5]} /> </SwiperSlide>
+                                        <SwiperSlide> <MovieCard movie={tv[6]} /> </SwiperSlide>
+                                        <SwiperSlide> <MovieCard movie={tv[7]} /> </SwiperSlide>
+                                        <SwiperSlide> <MovieCard movie={tv[8]} /> </SwiperSlide>
+                                        <SwiperSlide> <MovieCard movie={tv[9]} /> </SwiperSlide>
+                                        </Swiper> : windowSize.length === 425 ? <Swiper
+                                            modules={[Navigation, Pagination, Scrollbar, A11y]}
+                                            spaceBetween={0}
+                                            slidesPerView={2}
+                                            style={{ width: 425, height: 490 }}
+                                            loop
+                                        >
+                                            <SwiperSlide> <MovieCard movie={tv[0]} /> </SwiperSlide>
+                                            <SwiperSlide> <MovieCard movie={tv[1]} /> </SwiperSlide>
+                                            <SwiperSlide> <MovieCard movie={tv[2]} /> </SwiperSlide>
+                                            <SwiperSlide> <MovieCard movie={tv[3]} /> </SwiperSlide>
+                                            <SwiperSlide> <MovieCard movie={tv[4]} /> </SwiperSlide>
+                                            <SwiperSlide> <MovieCard movie={tv[5]} /> </SwiperSlide>
+                                            <SwiperSlide> <MovieCard movie={tv[6]} /> </SwiperSlide>
+                                            <SwiperSlide> <MovieCard movie={tv[7]} /> </SwiperSlide>
+                                            <SwiperSlide> <MovieCard movie={tv[8]} /> </SwiperSlide>
+                                            <SwiperSlide> <MovieCard movie={tv[9]} /> </SwiperSlide>
+                                        </Swiper> : null}
+                                </div>
+                            </div>
+
                         </div>
                     </div>
-
                 </div>
-            </div>
-        );
+            </>
     }
 }
 
