7197abd14d56bed08e8be43ebba9be812f339a28
Smajli3 <amer.smajli@icloud.com>
2024-02-26 16:55:50 +0100

    Update component -> Make better responsive

--- src/actors/Actors.jsx
+++ src/actors/Actors.jsx
@@ -17,23 +17,129 @@ function Actors({stars}) {
     if(!stars || stars.length === 0) {
         return null 
     } else {
+        const starsArray1 = [];
+        const starsArray2 = [];
+        const starsArray3 = [];
+
+        for(let i = 0; i < 6; i++) {
+            starsArray1.push(stars[i]);
+        }
+        for (let i = 6; i < 12; i++) {
+            starsArray2.push(stars[i]);
+        }
+        for (let i = 12; i < 18; i++) {
+            starsArray3.push(stars[i]);
+        }
             return <>
                 <div className='actorsContainer'>
                     <div className='actorsContainerHeading'>Actors</div>
                     <div className='actorsContainerSubheading'>Explore different actors</div>
-                    <div className='actorsProfilesContainer'>
-                        <Swiper
+                    <div className='actorsProfilesWrapper'>
+                        {windowSize.width === 1024 ? <Swiper
                             modules={[Navigation, Pagination, Scrollbar, A11y]}
                             spaceBetween={0}
-                            width={420}
-                            slidesPerView={2}
-                            style={{ width: 1250 }}
+                            slidesPerView={1}
+                            style={{ width: 1024 }}
                             navigation
+                            loop
                         >
-                            {stars.map((s) => {
-                                return <SwiperSlide> <ActorProfile key={s.id} actor={s}/> </SwiperSlide>
-                            })}
-                        </Swiper>
+                            <SwiperSlide>
+                                <div className='actorsProfilesContainer'>
+                                    <div className='actorsProfilesContainerContent'>
+                                        {starsArray1.map((s) => {
+                                            return <ActorProfile key={s.id} actor={s} />
+                                        })}
+                                    </div>
+                                </div>
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <div className='actorsProfilesContainer'>
+                                    <div className='actorsProfilesContainerContent'>
+                                        {starsArray2.map((s) => {
+                                            return <ActorProfile key={s.id} actor={s} />
+                                        })}
+                                    </div>
+                                </div>
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <div className='actorsProfilesContainer'>
+                                    <div className='actorsProfilesContainerContent'>
+                                        {starsArray3.map((s) => {
+                                            return <ActorProfile key={s.id} actor={s} />
+                                        })}
+                                    </div>
+                                </div>
+                            </SwiperSlide>
+                        </Swiper> : windowSize.width === 768 ? <Swiper
+                            modules={[Navigation, Pagination, Scrollbar, A11y]}
+                            spaceBetween={0}
+                            slidesPerView={4}
+                            style={{ width: 768, height: 250 }}
+                            loop
+                        >
+                            <SwiperSlide> <ActorProfile actor={stars[0]}/> </SwiperSlide>
+                            <SwiperSlide> <ActorProfile actor={stars[1]}/> </SwiperSlide>
+                            <SwiperSlide> <ActorProfile actor={stars[2]}/> </SwiperSlide>
+                            <SwiperSlide> <ActorProfile actor={stars[3]}/> </SwiperSlide>
+                            <SwiperSlide> <ActorProfile actor={stars[4]}/> </SwiperSlide>
+                            <SwiperSlide> <ActorProfile actor={stars[5]}/> </SwiperSlide>
+                            <SwiperSlide> <ActorProfile actor={stars[6]}/> </SwiperSlide>
+                            <SwiperSlide> <ActorProfile actor={stars[7]}/> </SwiperSlide>
+                            <SwiperSlide> <ActorProfile actor={stars[8]}/> </SwiperSlide>
+                            <SwiperSlide> <ActorProfile actor={stars[9]}/> </SwiperSlide>
+                        </Swiper> : windowSize.width === 425 ? <Swiper
+                            modules={[Navigation, Pagination, Scrollbar, A11y]}
+                            spaceBetween={0}
+                            slidesPerView={2}
+                            style={{ width: 420, height: 250 }}
+                            loop
+                        >
+                            <SwiperSlide> <ActorProfile actor={stars[0]} /> </SwiperSlide>
+                            <SwiperSlide> <ActorProfile actor={stars[1]} /> </SwiperSlide>
+                            <SwiperSlide> <ActorProfile actor={stars[2]} /> </SwiperSlide>
+                            <SwiperSlide> <ActorProfile actor={stars[3]} /> </SwiperSlide>
+                            <SwiperSlide> <ActorProfile actor={stars[4]} /> </SwiperSlide>
+                            <SwiperSlide> <ActorProfile actor={stars[5]} /> </SwiperSlide>
+                            <SwiperSlide> <ActorProfile actor={stars[6]} /> </SwiperSlide>
+                            <SwiperSlide> <ActorProfile actor={stars[7]} /> </SwiperSlide>
+                            <SwiperSlide> <ActorProfile actor={stars[8]} /> </SwiperSlide>
+                            <SwiperSlide> <ActorProfile actor={stars[9]} /> </SwiperSlide>
+                            </Swiper> : <Swiper
+                                    modules={[Navigation, Pagination, Scrollbar, A11y]}
+                                    spaceBetween={0}
+                                    slidesPerView={1}
+                                    style={{ width: 1250 }}
+                                    navigation
+                                    loop
+                                >
+                                    <SwiperSlide>
+                                        <div className='actorsProfilesContainer'>
+                                            <div className='actorsProfilesContainerContent'>
+                                                {starsArray1.map((s) => {
+                                                    return <ActorProfile key={s.id} actor={s} />
+                                                })}
+                                            </div>
+                                        </div>
+                                    </SwiperSlide>
+                                    <SwiperSlide>
+                                        <div className='actorsProfilesContainer'>
+                                            <div className='actorsProfilesContainerContent'>
+                                                {starsArray2.map((s) => {
+                                                    return <ActorProfile key={s.id} actor={s} />
+                                                })}
+                                            </div>
+                                        </div>
+                                    </SwiperSlide>
+                                    <SwiperSlide>
+                                        <div className='actorsProfilesContainer'>
+                                            <div className='actorsProfilesContainerContent'>
+                                                {starsArray3.map((s) => {
+                                                    return <ActorProfile key={s.id} actor={s} />
+                                                })}
+                                            </div>
+                                        </div>
+                                    </SwiperSlide>
+                                </Swiper>}
                     </div>
                 </div>
             </>
